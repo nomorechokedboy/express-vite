12ef3e1881674026be21285f0fea63100c3ab38b
nomorechokedboy <haiduongit@protonmail.com>
2022-06-27 21:49:44 +0700

    edit eslintrc compose file and configs/db

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -1,14 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-export default function connectDb() {
-  MONGODB &&
-    mongoose
-      .connect(MONGODB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      } as mongoose.ConnectOptions)
-      .then(async (_: typeof mongoose) => {
-        console.log('Connect Mongodb Success!');
-      })
-      .catch((e) => console.error(e));
-}
+export default new PrismaClient();
